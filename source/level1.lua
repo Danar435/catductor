466705c8f1b5bd9107918efe710666073ec0d52d
Kristiyan <2003kristiyan@gmail.com>
2026-09-22 12:15:08 +0200

    Add level

---
+++ source/level1.lua
@@ -0,0 +1,24 @@
+import "CoreLibs/graphics"
+
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+local obstacles = { 30, 10, 10, 20, 5, 5, 5, 10, 20 }
+local time = 100
+
+local function sumDistance(obstacles)
+    local sum = 0
+    for  i, obstacle in ipairs(obstacles) do
+        sum = sum + obstacle
+    end
+    return sum
+end
+
+Level1 = {
+    
+    obstacles = obstacles,
+    distance = sumDistance(obstacles),
+    time = time
+
+}
+
